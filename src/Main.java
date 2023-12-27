1e903f4b622dfa074b6388669a306bfbd19d123d
RelancioBorgesDev <relancio.dev@gmail.com>
2023-12-27 09:31:25 -0300

    leetcode 2185

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println(sol.sumOfUnique(new int[]{1,2,3,2}));
+        System.out.println(sol.prefixCount(new String[]{"pay","attention","practice","attend"}, "at"));
+        System.out.println(sol.prefixCount(new String[]{"leetcode","win","loops","success"}, "code"));
     }
 }
 
@@ -27,5 +29,15 @@ class Solution {
         }
 
         return uniques.stream().mapToInt(Integer::intValue).sum();
+    }
+    //2185
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        for (String word : words) {
+            if (word.startsWith(pref)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
