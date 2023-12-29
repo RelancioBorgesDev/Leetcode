069a19dbeefe117d95347777cbe62b45e1bd017b
RelancioBorgesDev <relancio.dev@gmail.com>
2023-12-29 11:29:02 -0300

    leetcode 2956

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println(sol.prefixCount(new String[]{"pay","attention","practice","attend"}, "at"));
         System.out.println(sol.prefixCount(new String[]{"leetcode","win","loops","success"}, "code"));
         System.out.println(Arrays.toString(sol.findArray(new int[]{5, 2, 0, 3, 1})));
+        System.out.println(Arrays.toString(sol.findIntersectionValues(new int[]{4, 3, 2, 3, 1}, new int[]{2, 2, 5, 2, 3, 6})));
 
     }
 }
@@ -52,4 +53,38 @@ class Solution {
         }
         return result;
     }
+
+    //2956. Find Common Elements Between Two Arrays
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        for (int num : nums2) {
+            set2.add(num);
+        }
+
+        int count1 = countOccurrences(set1, nums2);
+        int count2 = countOccurrences(set2, nums1);
+
+        return new int[]{count2, count1};
+    }
+
+    private int countOccurrences(Set<Integer> set, int[] nums) {
+        int count = 0;
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                count++;
+
+            }
+        }
+
+        return count;
+    }
+
+
 }
